654e5538ff4b49a9b060bc57086adc1a9a9ab7de
Leonardo Guarino <leonardo.guarino@piksel.com>
2023-09-14 23:16:43 +0100

    Prep3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string repeat = "yes";
+
+        while (repeat == "yes")
+        {
+            Console.Write("What is your guess number? Choose between 1 and 10: ");
+            string guess = Console.ReadLine();
+            int guess_int = int.Parse(guess); 
+
+            //create the ramdon number
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+            int attemps= 0;
+
+            while (number != guess_int)
+            {
+                attemps++;
+                if (guess_int < number)
+                {
+                    Console.Write("Higher\n");
+                    Console.Write("What is your number? ");
+                    guess = Console.ReadLine();
+                    guess_int = int.Parse(guess); 
+                }
+                else
+                {
+                    Console.Write("Lower\n");
+                    Console.Write("What is your number? ");
+                    guess = Console.ReadLine();
+                    guess_int = int.Parse(guess); 
+                }
+            }
+
+        Console.Write($"You guessed it in {attemps} attemps!\n");
+        Console.Write("Do you want to play again? yes/no : ");
+        repeat = Console.ReadLine();
+
+        }
+    Console.Write("Thank you for playing \n");
     }
 }
