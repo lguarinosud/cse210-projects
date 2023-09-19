7c6a0323e54826eab33278935fe672cadc03f8f0
Leonardo Guarino <leonardo.guarino@piksel.com>
2023-09-19 22:42:12 +0100

    Learning 2 completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job Job1 = new Job(); 
+        Job1._jobTitle = "Software Engineer";
+        Job1._company = "Piksel";
+        Job1._startYear = 2019;
+        Job1._endYear = 2022;  
+
+        Job Job2 = new Job();
+        Job2._jobTitle = "Devops";
+        Job2._company = "RCN";
+        Job2._startYear = 2022;
+        Job2._endYear = 2023;
+
+       
+        //Job1.Display();
+        //Job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Leonardo Guarino";
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(Job2);
+
+        myResume.Display();
     }
 }
